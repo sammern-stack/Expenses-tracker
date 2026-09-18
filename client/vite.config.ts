9115e56798c6bca75f12c64a6c6736f485dfc128
sammern <samreactdev@gmail.com>
2026-09-18 10:52:43 +0300

    feat: initialized client with vite
    - installed necessary dependencies
    - configured vite.config.ts with path alias for clean imports and auto insert scss mixins file for every scss file, added svgr plugin
    - configure tsconfig.app.json to include path alias
    - updated index.html with proper title and new favicon
    - updated .gitignore
    - remove unused css files and clear template code from App.tsx
    - create vite-env.d.ts types file to enable svgr plugin
    - added all necessary assets for the project

---
+++ client/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import svgr from "vite-plugin-svgr";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: (src: string, filename: string) => {
+          const normalize = filename.replace("/\\/", "/");
+          const shouldExclude = normalize.includes("/shared/styles");
+
+          if (shouldExclude) return src;
+          return `@use "@/shared/styles/mixins" as *;\n${src}`;
+        },
+      },
+    },
+  },
+});
